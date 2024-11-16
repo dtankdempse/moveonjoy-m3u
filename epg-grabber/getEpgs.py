36dedf3571e97147931c6252892fe42b4b97414c
Reuben <dtankdempsey@gmail.com>
2024-11-16 10:10:08 -0700

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -62,6 +62,7 @@ def filter_and_build_epg(urls):
 urls = [
     'https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
+    'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
 ]
 
 if __name__ == "__main__":
