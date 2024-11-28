1987ca904df40629a6fed648ec8828ed28a5a175
Reuben <dtankdempsey@gmail.com>
2024-11-28 09:36:55 -0700

    Update getEpgs.py

--- epg-grabber/getEpgs.py
+++ epg-grabber/getEpgs.py
@@ -64,6 +64,7 @@ urls = [
     'https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_CA1.xml.gz',
     'https://epgshare01.online/epgshare01/epg_ripper_PLEX1.xml.gz',
+    'https://bit.ly/103216-595281-xml-gz',
 ]
 
 if __name__ == "__main__":
